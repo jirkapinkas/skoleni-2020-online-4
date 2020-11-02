30ee55ffdf80c1df5307ae531fa8784fa261b2fd
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-02 11:13:26 +0100

    rozsireni hello world prikladu o alternativni konfiguraci + komentare

--- hello-world/src/main/java/com/test/app/Main.java
+++ hello-world/src/main/java/com/test/app/Main.java
@@ -1,15 +1,22 @@
 package com.test.app;
 
+import com.test.app.repository.ItemRepository;
 import com.test.app.service.HelloService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan // component scan mechanismus: abychom nemuseli rucne zapojovat vsechny Spring beany do Spring kontejneru
+               // kdyz u @ComponentScan neni definovany balicek, ktery se bude scanovat, tak se bude scanovat aktualni balicek (a jeho podbalicky)
+//@ComponentScan("com.test.app") // toto je to same jako @ComponentScan v tomto pripade!!!
 public class Main {
 
     public static void main(String[] args) {
+        // tohle by take fungovalo:
+//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(HelloService.class, ItemRepository.class);
+
+        // tohle je Spring kontejner, v cele aplikaci je 1x!!!!!!
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Main.class);
 
         HelloService helloService = applicationContext.getBean(HelloService.class);
