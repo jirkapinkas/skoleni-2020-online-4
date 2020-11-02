f65e563401130fcfc2a7612efe0d21306765edce
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-02 12:30:21 +0100

    + lombok & constructor dependency injection

--- hello-world/src/main/java/com/test/app/service/HelloService.java
+++ hello-world/src/main/java/com/test/app/service/HelloService.java
@@ -1,14 +1,19 @@
 package com.test.app.service;
 
 import com.test.app.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+//@RequiredArgsConstructor
 public class HelloService {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+
+    public HelloService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     public String getCount() {
         return "Number of items: " + itemRepository.count();
