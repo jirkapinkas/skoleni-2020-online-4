653340050d739e2845e848ed57449dee2822a596
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-03 09:17:37 +0100

    + spring boot hello world

---
+++ eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.eshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopWebApplication.class, args);
+    }
+
+}
