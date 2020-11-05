2e7f19f190f1e5b909f9deeb1f31324db4658a6b
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-05 09:30:36 +0100

    + ItemService (interface) + ItemServiceImpl (implementuje ItemService interface) ... kvuli prikladu na pochopeni proxy design pattern

--- eshop-web/src/main/java/com/example/eshopweb/service/ItemService.java
+++ eshop-web/src/main/java/com/example/eshopweb/service/ItemService.java
@@ -1,60 +1,18 @@
 package com.example.eshopweb.service;
 
 import com.example.eshopweb.dto.ItemDto;
-import com.example.eshopweb.entity.Item;
-import com.example.eshopweb.exception.DeleteException;
-import com.example.eshopweb.exception.NotFoundException;
-import com.example.eshopweb.mapper.ItemMapper;
-import com.example.eshopweb.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class ItemService {
+public interface ItemService {
+    List<ItemDto> findAll(Sort sort);
 
-    @Autowired
-    private ItemRepository itemRepository;
+    Optional<ItemDto> findById(int id);
 
-    @Autowired
-    private ItemMapper itemMapper;
+    ItemDto save(ItemDto itemDto);
 
-    @Transactional(readOnly = true)
-    public List<ItemDto> findAll(Sort sort) {
-        List<Item> items = itemRepository.findAll(sort);
-//        List<Item> items = itemRepository.findAllFetchCategory(sort);
-        return itemMapper.entityToDto(items);
-    }
-
-    @Transactional(readOnly = true)
-    public Optional<ItemDto> findById(int id) {
-        return itemRepository.findById(id)
-//        return itemRepository.findByIdFetchCategory(id)
-                .map(itemMapper::entityToDto); // od Java 8: method reference
-//                .map(item -> itemMapper.entityToDto(item)); // od Java 8: lambda
-    }
-
-    @Transactional
-    public ItemDto save(ItemDto itemDto) {
-        Item item = itemMapper.dtoToEntity(itemDto);
-        Item managedItem = itemRepository.save(item); // managedItem obsahuje hodnotu "id"
-        return itemMapper.entityToDto(managedItem);
-    }
-
-    @Transactional
-    public void deleteById(int id) {
-        if(!itemRepository.existsById(id)) {
-            throw new NotFoundException("Item with id: " + id + " does not exist!");
-        }
-        try {
-            itemRepository.deleteById(id);
-        } catch (Exception e) {
-            throw new DeleteException("Couldn't delete item with id: " + id);
-        }
-    }
+    void deleteById(int id);
 
 }
---
+++ eshop-web/src/main/java/com/example/eshopweb/service/ItemServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.eshopweb.service;
+
+import com.example.eshopweb.dto.ItemDto;
+import com.example.eshopweb.entity.Item;
+import com.example.eshopweb.exception.DeleteException;
+import com.example.eshopweb.exception.NotFoundException;
+import com.example.eshopweb.mapper.ItemMapper;
+import com.example.eshopweb.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ItemDto> findAll(Sort sort) {
+        List<Item> items = itemRepository.findAll(sort);
+//        List<Item> items = itemRepository.findAllFetchCategory(sort);
+        return itemMapper.entityToDto(items);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<ItemDto> findById(int id) {
+        return itemRepository.findById(id)
+//        return itemRepository.findByIdFetchCategory(id)
+                .map(itemMapper::entityToDto); // od Java 8: method reference
+//                .map(item -> itemMapper.entityToDto(item)); // od Java 8: lambda
+    }
+
+    @Override
+    @Transactional
+    public ItemDto save(ItemDto itemDto) {
+        Item item = itemMapper.dtoToEntity(itemDto);
+        Item managedItem = itemRepository.save(item); // managedItem obsahuje hodnotu "id"
+        return itemMapper.entityToDto(managedItem);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        if(!itemRepository.existsById(id)) {
+            throw new NotFoundException("Item with id: " + id + " does not exist!");
+        }
+        try {
+            itemRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new DeleteException("Couldn't delete item with id: " + id);
+        }
+    }
+
+}
