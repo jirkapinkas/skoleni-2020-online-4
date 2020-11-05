c9e5b690e53ead27e085bb21736a80b233dd3d4b
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-05 14:28:38 +0100

    + integracni testy pomoci Spring + AssertJ

--- eshop-web/src/test/java/com/example/eshopweb/EshopWebApplicationTests.java
+++ eshop-web/src/test/java/com/example/eshopweb/EshopWebApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.eshopweb;
 
+import com.example.eshopweb.dto.ItemDto;
+import com.example.eshopweb.service.ItemService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class EshopWebApplicationTests {
 
+    @Autowired
+    private ItemService itemService;
+
+    // integracni test
     @Test
     void contextLoads() {
+        List<ItemDto> items = itemService.findAll(Sort.by("id"));
+        // https://assertj.github.io/doc/
+        assertThat(items)
+                .isNotNull()
+                .isNotEmpty()
+                .extracting(ItemDto::getName)
+                .contains("Java in 21 days", "Java cup");
     }
 
 }
