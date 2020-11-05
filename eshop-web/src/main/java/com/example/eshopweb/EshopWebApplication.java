8720bee03083585b598492a56530ab5382466069
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-05 12:12:20 +0100

    priklad na @Scheduled

--- eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
+++ eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
@@ -1,11 +1,15 @@
 package com.example.eshopweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /*
 Anotace @SpringBootApplication v sobe schovava:
@@ -14,13 +18,21 @@ Anotace @SpringBootApplication v sobe schovava:
 @EnableAutoConfiguration
 */
 @EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class EshopWebApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(EshopWebApplication.class);
 
     public static void main(String[] args) {
         // args se da vyuzit:
         // java -jar target/app.jar --server.port=9090
         SpringApplication.run(EshopWebApplication.class, args);
     }
+
+//    @Scheduled(fixedDelay = 5_000)
+//    public void run() {
+//        log.info("running");
+//    }
 
 }
