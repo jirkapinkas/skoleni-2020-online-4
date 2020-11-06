230e9f332ebb0513f40b6be510835c0ea1e4f3c9
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-06 11:01:56 +0100

    alternativni zpusob zapojeni DataSource pomoci custom @Bean (low-level konfigurace pripojeni do databaze)

--- eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
+++ eshop-web/src/main/java/com/example/eshopweb/EshopWebApplication.java
@@ -1,13 +1,16 @@
 package com.example.eshopweb;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -27,6 +30,20 @@ Anotace @SpringBootApplication v sobe schovava:
 public class EshopWebApplication {
 
     private static final Logger log = LoggerFactory.getLogger(EshopWebApplication.class);
+
+//    @Bean
+//    public HikariDataSource dataSource(
+//            @Value("${spring.datasource.url}") String url,
+//            @Value("${spring.datasource.username}") String username,
+//            @Value("${spring.datasource.password}") String password
+//    ) {
+//        log.info("Datasource created!");
+//        HikariDataSource dataSource = new HikariDataSource();
+//        dataSource.setJdbcUrl(url);
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+//        return dataSource;
+//    }
 
     public static void main(String[] args) {
         // args se da vyuzit:
